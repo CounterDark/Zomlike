9235cee8586479a090df546280af752636512a2c
filippat <58233857+Flopp0@users.noreply.github.com>
2025-06-16 12:59:24 +0200

    Collisions and path finding good improvements

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -3,17 +3,22 @@ class_name Player
 
 @export var max_speed: float = 400
 @export var min_speed: float = 40
+@export var collision_speed: float = 1000
+
+const GAME_OVER_SCENE : PackedScene = preload("res://Scenes/main_menu/MainMenu.tscn")
 
 @onready var reduced_speed_timer : Timer = $Timers/ReducedSpeedTimer
 @onready var collision_timer : Timer = $Timers/CollisionTimer
 
 var collided : bool = false
+var can_collide : bool = true
+
 var equipped_weapon : Weapon
 
 var speed: float = max_speed :
 	set(value):
 		if value < speed:
-			speed = min(value, min_speed)
+			speed = max(value, min_speed)
 		else:
 			speed = min(value, max_speed)
 		
@@ -25,28 +30,35 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if PlayerStats.health < 0 :
+		get_tree().change_scene_to_packed(GAME_OVER_SCENE)
 	if Input.is_action_pressed("primary action"):
 		if equipped_weapon != null:
 			equipped_weapon.attack()
 
 func _physics_process(_delta: float) -> void:
-	var direction : Vector2 = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
+	if collided:
+		collided = false
+	else:	
+		var direction : Vector2 = Input.get_vector("left", "right", "up", "down")
+		velocity = direction * speed
+		Globals.player_direction = direction
 	move_and_slide()
 	Globals.player_position = global_position
-	Globals.player_direction = direction
 
 func _on_player_collided(_position : Vector2, collision_direction : Vector2):
-	if !collided :
+	if can_collide :
 		collided = true
+		can_collide = false
 		speed -= 50
 		collision_timer.start()
 		reduced_speed_timer.start()
-
+		collision_direction = -1 * collision_direction
+		velocity = collision_speed * collision_direction
+		Globals.player_direction = collision_direction
 
 func _on_reduced_speed_timer_timeout() -> void:
 	speed = max_speed
 
-
 func _on_collision_timer_timeout() -> void:
-	collided = false
+	can_collide = true
--- scripts/characters/zombie.gd
+++ scripts/characters/zombie.gd
@@ -1,43 +1,50 @@
 extends CharacterBody2D
 
+@export var default_speed: int = 300
 @export var speed: int = 300
 @export var finding_speed: int = 300
 @export var damage : int = 10
 @export var crit_chance : float = 0.05
 @export var crit_multiplier : float = 1.5
+@export var bounce_multiplier : float = 3.0
 @export var health: int = 30
 @onready var navigation_agent: NavigationAgent2D = $ZombieNavigation
 @onready var vurnerable_timer: Timer = $Timers/VurnerableTimer
 @onready var attack_timer: Timer = $Timers/AttackTimer
+@onready var walk_timer: Timer = $Timers/WalkTimer
 
 var can_attack : bool = true
+var can_walk : bool = true
 var enemy_nearby : bool = false
 var vulnerable: bool = true
+var collided: bool = true
 
 func _ready():
 	navigation_agent.target_position = Globals.player_position
 	
-
 func _physics_process(delta: float) -> void:
-	var direction: Vector2
-	if !enemy_nearby:
-		var next_path_pos: Vector2 = navigation_agent.get_next_path_position()
-		direction = (next_path_pos - global_position).normalized()
-		velocity = direction * finding_speed * delta
-	else:
-		direction = (Globals.player_position - position).normalized()
-		velocity = direction * speed * delta
+	if can_walk:
+		var direction: Vector2
+		if !enemy_nearby:
+			var next_path_pos: Vector2 = navigation_agent.get_next_path_position()
+			direction = (next_path_pos - global_position).normalized()
+			velocity = direction * finding_speed * delta
+		else:
+			direction = (Globals.player_position - position).normalized()
+			velocity = direction * speed * delta
 		
-	var collision : KinematicCollision2D = move_and_collide(velocity)
-	
-	if collision:
-		var reflect = collision.get_remainder().bounce(collision.get_normal())
-		velocity = velocity.bounce(collision.get_normal())
-		move_and_collide(reflect)
-		var collider : Object = collision.get_collider()
-		if collider is Player:
-			SignalBus.player_collided.emit(global_position, direction)
-
+		var collision : KinematicCollision2D = move_and_collide(velocity)
+		
+		if collision:
+			var collision_direction = collision.get_normal()
+			var reflect = collision.get_remainder().bounce(direction)
+			velocity = velocity.bounce(collision.get_normal()) * speed * delta * bounce_multiplier
+			move_and_collide(reflect)
+			var collider : Object = collision.get_collider()
+			if collider is Player:
+				SignalBus.player_collided.emit(global_position, collision_direction)
+				walk_timer.start()
+				can_walk = false
 
 func hit(hit_damage : int):
 	if vulnerable:
@@ -69,6 +76,8 @@ func _on_vurnerable_timer_timeout() -> void:
 func _on_attack_timer_timeout() -> void:
 	can_attack = true
 
-
 func _on_navigation_timer_timeout() -> void:
 	navigation_agent.target_position = Globals.player_position
+
+func _on_walk_timer_timeout() -> void:
+	can_walk = true
